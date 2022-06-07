ef83b741f3828d86aaeb263ff8bf4fbb82966f82
munawwirul jamal <munawwirul.jamal@gmail.com>
2022-06-07 22:40:14 +0700

    feat (db) configuration for gorm

---
+++ db.go
@@ -0,0 +1,28 @@
+package nogosari
+
+import (
+	"fmt"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func (a *app) initDb() {
+	if (a.DbConf.Dialect == "") || (a.DbConf.Dsn == "") {
+		return
+	}
+	var gormD gorm.Dialector
+	if a.DbConf.Dialect == "mysql" {
+		gormD = postgres.Open(a.DbConf.Dsn)
+	} else if a.DbConf.Dialect == "postgres" {
+		gormD = mysql.Open(a.DbConf.Dsn)
+	}
+
+	if db, err := gorm.Open(gormD); err != nil {
+		panic("Failed to connect to database!")
+	} else {
+		fmt.Println("Connected to database: " + db.Name())
+		a.DB = db
+	}
+}
